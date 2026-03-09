cc37294e2680b2711e9fcfb78d9c469f044b664c
Jeroen-MB-hub <jeroen.turnen@gmail.com>
2026-03-10 00:04:15 +0100

    main.ts, main.blocks bijwerken

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 let Graden = 0
 basic.forever(function () {
     Graden = input.compassHeading()
-    if (Graden < 45) {
+    if (Graden <= 45) {
         basic.showString("N")
-    } else if (Graden < 90) {
+    } else if (Graden <= 90) {
         basic.showString("O")
-    } else if (Graden < 180) {
+    } else if (Graden <= 180) {
         basic.showString("Z")
-    } else if (Graden < 270) {
+    } else if (Graden <= 270) {
         basic.showString("W")
     } else {
     	
